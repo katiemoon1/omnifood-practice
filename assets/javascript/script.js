f16086e1f05237d2f2aeae8f707e71b0cf051028
Katie Moon <kaitline.moon@gmail.com>
2019-07-22 10:05:49 -0400

    started adding animations

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -76,4 +76,14 @@ $(document).ready(function() {
         }
       }
     });
+
+  // Animations on scroll
+  $(".js-wp-1").waypoint(
+    function(direction) {
+      $(".js-wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
 });
